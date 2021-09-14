1a0e50ff1ab47874461ed23762bdd14b16a27a6c
Thiago <Cliente@DESKTOP-DDOLVP6>
2021-09-14 20:35:36 -0300

    Correção Erro

--- lib/app/views/homePage_MenuRepresentante.dart
+++ lib/app/views/homePage_MenuRepresentante.dart
@@ -16,7 +16,145 @@ class _MyHomePageState extends State<HomePage_MenuRepresentante> {
       appBar: AppBar(
         title: Text(widget.title, style: TextStyle(color: Colors.white)),
       ),
-      body: Center(),
+      body: ListView(
+        children: [
+          Container(
+            padding: new EdgeInsets.all(30),
+            child: Center(
+              child: Text("Cotações para Responder",
+                  style: TextStyle(fontSize: 30, color: Colors.orange[300])),
+            ),
+          ),
+          Container(
+            child: new InkWell(
+              onTap: () {},
+              child: Card(
+                color: Colors.white,
+                child: Container(
+                  margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.02,
+                    right: MediaQuery.of(context).size.width * 0.04,
+                    left: MediaQuery.of(context).size.width * 0.04,
+                    bottom: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(
+                              child: Text("Produto ",
+                                  style: TextStyle(
+                                      fontSize: 19,
+                                      fontWeight: FontWeight.bold))),
+                          Text('Enviado em',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black87))
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(
+                            child: Container(
+                              padding: new EdgeInsets.only(left: 5, top: 7),
+                              child: Text("35 Produtos para responder",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.black54,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                          ),
+                          Container(
+                            child: Container(
+                                padding: new EdgeInsets.only(top: 5),
+                                child: Text('14/09/2021',
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.red))),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: new EdgeInsets.all(30),
+            child: Center(
+              child: Text("Cotações Respondidas",
+                  style: TextStyle(fontSize: 30, color: Colors.orange[300])),
+            ),
+          ),
+          for (int i = 0; i < 10; i++)
+            Container(
+              child: new InkWell(
+                onTap: () {},
+                child: Card(
+                  color: Colors.white,
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.02,
+                      right: MediaQuery.of(context).size.width * 0.04,
+                      left: MediaQuery.of(context).size.width * 0.04,
+                      bottom: MediaQuery.of(context).size.height * 0.02,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                                child: Text("Produto ",
+                                    style: TextStyle(
+                                        fontSize: 19,
+                                        fontWeight: FontWeight.bold))),
+                            Text('Enviado em',
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black87))
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Expanded(
+                              child: Container(
+                                padding: new EdgeInsets.only(left: 5, top: 7),
+                                child: Text("35 Produtos para responder",
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.black54,
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                            ),
+                            Container(
+                              child: Container(
+                                  padding: new EdgeInsets.only(top: 5),
+                                  child: Text('14/09/2021',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.green))),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
