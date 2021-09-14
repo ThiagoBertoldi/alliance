0cc09c47f5ff1c26c9f7b0e5894d289746466651
hayanavieira <hayana.vieira46@gmail.com>
2021-09-13 21:18:21 -0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 
+
+
 import 'tabs_page.dart';
 
 void main() async {
